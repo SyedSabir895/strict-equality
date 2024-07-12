36812d06650d946aa3f0aa56be40fdce8a8c4853
SyedSabir895 <sabirsyed2005@gmail.com>
2024-07-12 12:10:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// = assignment operator
+// == comparison operator(used to compare two values)
+// === strict equality operator(used to compare if values and datatypes are equal)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = "3.14";
+// in this if else statement we assigning variables and checking whether it is right or not but if we keep === it will show the else statement
+// so we should change the variable in to the numeric data type by using strict equlity operator we can compare if data types are equal or not
+if(PI !== "3.14"){
+    console.log("It is  NOT PI");
+}
+
+else{
+    console.log("It is PI");
+}
